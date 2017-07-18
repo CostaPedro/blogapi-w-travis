80d23f98ce09ac93762d05e7f1118a9fb4a1df8e
CostaPedro <pedromcosta.gr@hotmail.com>
2017-07-18 12:50:27 -0400

    add author to test

--- test/test_w_travis.js
+++ test/test_w_travis.js
@@ -74,7 +74,8 @@ describe('Blog Posts', function() {
       .then(function( res) {
         const updatedPost = Object.assign(res.body[0], {
           title: 'connect the dots',
-          content: 'la la la la la'
+          content: 'la la la la la',
+          author: 'pedro costa'
         });
         return chai.request(app)
           .put(`/blog-posts/${res.body[0].id}`)
